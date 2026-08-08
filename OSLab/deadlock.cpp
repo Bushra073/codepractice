0f0de2fc2af4cc862fddffad6882beb55683d2c0
Bushra073 <bushrasalsabil43@gmail.com>
2026-08-08 21:38:43 +0600

    update

--- OSLab/deadlock.cpp
+++ OSLab/deadlock.cpp
@@ -1,85 +1,273 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int n,m;
-    cout << "Enter number of processes: ";
+    cout << "Enter no. of processes: ";
     cin >> n;
-    cout << "Enter number of resources: ";
+     cout << "Enter no. of resources: ";
     cin >> m;
 
     int alloc[100][100];
     int max[100][100];
-    int need[100][100];
+    int need [100][100];
     int avail[100];
 
-    cout <<"Enter Allocation Matrix: " << endl;
+    cout << "\nEnter Allocation Matrix:\n " ;
     for(int i=0; i<n; i++){
         cout << "P" << i << ": ";
-
         for(int j=0; j<m; j++){
             cin >> alloc[i][j];
         }
     }
-    cout << "Enter Maximum Matrix: " << endl;
-    for(int i=0; i<n; i++){
+    cout << "\nEnter Maximum Matrix:\n " ;
+
+    for(int i=0; i<n; i++)
+        {
         cout << "P" << i << ": ";
-        for(int j=0; j<m; j++){
-            cin >> max[i][j];
+
+        for(int j=0; j<m; j++)
+            {
+            cin >> max [i][j];
         }
     }
-    cout << "Enter Available Resources: " << endl;
+    cout << "\nEnter Available Resources:\n " ;
     for(int j=0; j<m; j++){
         cin >> avail[j];
     }
+
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            need[i][j] = max[i][j]- alloc[i][j];
+            need[i][j] = max[i][j] - alloc[i][j];
         }
     }
+
     bool finish[100] = {false};
     int safeSequence[100];
     int count = 0;
 
-    while(count<n){
-        bool found = false;
+    while(count < n){
 
+        bool found = false;
         for(int i=0; i<n; i++){
 
-            if(ifinish[i] == false){
+            if (finish[i] == false){
                 bool possible = true;
-
                 for(int j=0; j<m; j++){
-                    if(need[i][j] > avail[j]){
+                    if(need[i][j]>avail[j]){
                         possible = false;
                         break;
                     }
                 }
-
                 if(possible == true){
+
                     for(int j=0; j<m; j++){
                         avail[j] += alloc[i][j];
                     }
                     finish[i] = true;
                     safeSequence[count] = i;
-                    count ++ ;
+                    found = true;
+                    count ++;
+                }
+            }
+        }
+        if(found == false){
+            break;
+        }
+    }
+
+    if(count == n){
+
+        cout << "System is safe" << endl;
+
+        cout << "Safe Sequence: ";
+
+        for(int i=0; i<n; i++){
+            cout << "P" << safeSequence[i];
+        }
+    }
+
+    else {
+        cout << "System is UNSAFE " << endl;
+    }
+    return 0;
+}
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Take the number of processes.
+    int n;
+    cout << "Enter number of processes: ";
+    cin >> n;
+
+    // Take the number of resource types.
+    int m;
+    cout << "Enter number of resources: ";
+    cin >> m;
+
+    // Declare the required arrays.
+    // alloc = resources currently allocated to each process.
+    // max = maximum resources each process may need.
+    // need = remaining resources each process needs.
+    // avail = resources currently available.
+    int alloc[100][100];
+    int max[100][100];
+    int need[100][100];
+    int avail[100];
+
+    // Take the Allocation Matrix.
+    cout << "\nEnter Allocation Matrix:\n";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << "P" << i << ": ";
+
+        for(int j = 0; j < m; j++)
+        {
+            cin >> alloc[i][j];
+        }
+    }
+
+    // Take the Maximum Matrix.
+    cout << "\nEnter Maximum Matrix:\n";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << "P" << i << ": ";
+
+        for(int j = 0; j < m; j++)
+        {
+            cin >> max[i][j];
+        }
+    }
+
+    // Take the Available Resources.
+    cout << "\nEnter Available Resources:\n";
+
+    for(int j = 0; j < m; j++)
+    {
+        cin >> avail[j];
+    }
+
+    // Calculate Need.
+    // Need = Maximum - Allocation.
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            need[i][j] = max[i][j] - alloc[i][j];
+        }
+    }
+
+    // finish[i] tells whether process i has finished.
+    // Initially, no process has finished.
+    bool finish[100] = {false};
+
+    // Stores the order in which processes can safely finish.
+    int safeSequence[100];
+
+    // Counts how many processes have finished.
+    int count = 0;
+
+    // Keep checking until all processes are finished.
+    while(count < n)
+    {
+        // At the beginning of each round,
+        // we have not found any process yet.
+        bool found = false;
+
+        // Check every process.
+        for(int i = 0; i < n; i++)
+        {
+            // Only check processes that have not finished.
+            if(finish[i] == false)
+            {
+                // Assume this process can finish.
+                bool possible = true;
+
+                // Check every resource for this process.
+                for(int j = 0; j < m; j++)
+                {
+                    // If the process needs more resources
+                    // than are currently available,
+                    // it cannot finish right now.
+                    if(need[i][j] > avail[j])
+                    {
+                        possible = false;
+
+                        // No need to check the remaining resources.
+                        break;
+                    }
+                }
+
+                // If the process can get all the resources it needs.
+                if(possible == true)
+                {
+                    // The process finishes and returns
+                    // the resources it was holding.
+                    for(int j = 0; j < m; j++)
+                    {
+                        avail[j] += alloc[i][j];
+                    }
+
+                    // Mark this process as finished.
+                    finish[i] = true;
+
+                    // Store this process in the safe sequence.
+                    safeSequence[count] = i;
+
+                    // Increase the number of completed processes.
+                    count++;
+
+                    // We found a process that could finish.
                     found = true;
                 }
             }
         }
+
+        // If no process could finish in this round,
+        // the remaining processes cannot proceed.
+        if(found == false)
+        {
+            break;
+        }
     }
 
+    // If all processes finished, the system is safe.
+    if(count == n)
+    {
+        cout << "\nSystem is SAFE.\n";
 
+        // Print the safe sequence.
+        cout << "Safe Sequence: ";
 
+        for(int i = 0; i < n; i++)
+        {
+            cout << "P" << safeSequence[i];
 
+            // Print an arrow between processes,
+            // but not after the last process.
+            if(i != n - 1)
+            {
+                cout << " -> ";
+            }
+        }
 
+        cout << endl;
+    }
 
+    // If all processes did not finish,
+    // the system is unsafe.
+    else
+    {
+        cout << "\nSystem is UNSAFE.\n";
+    }
 
-
-
-
-
-
-
-
+    return 0;
 }
--- practice.cpp
+++ practice.cpp
@@ -1,1 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    cout << "Enter number of processes:";
+    cin >> n;
+
+    int p[100], at[100], bt[100];
+    int ct[100], tat[100], wt[100];
+
+    for(int i=0; i<n; i++){
+
+        p[i] = i+1;
+        cout << "Process P " << p[i] << endl;
+
+        cout << "Arrival Time: ";
+        cin >> at[i];
+        cout << "Burst Time:";
+        cin >> bt[i];
+    }
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(at[j] > at[j+1]){
+                swap(at[j],at[j+1]);
+                swap(bt[j],bt[j+1]);
+                swap(p[j],p[j+1]);
+            }
+        }
+    }
+
+    ct[0] = at[0] + bt[0];
+    for(int i=0; i<n; i++){
+        if(ct[i-1] < at[i]){
+            ct[i] = at[i] + bt[i];
+        }
+        else{
+            ct[i] = ct[i-1] + bt[i];
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        tat[i] = ct[i]- at[i];
+        wt[i] = tat[i] - bt[i];
+    }
+    double avgTAT, avgWT;
+    for(int i=0; i<n; i++){
+        avgTAT += tat[i];
+        avgWT += wt[i];
+    }
+    avgTAT = avgTAT/n;
+    avgWT = avgWT/n;
+
+    cout << "Average Turn Around Time: " << avgTAT << endl;
+    cout << "Average Waiting Time: " << avgWT << endl;
+
+    cout << "\nP\tAT\tBT\tCT\tTAT\tWT\n";
+    for(int i=0; i<n; i++){
+        cout << "P" << p[i] << "\t"
+                    << at[i] << "\t"
+                    << bt[i] << "\t"
+                    << ct[i] << "\t"
+                    << tat[i] << "\t"
+                    << wt[i] << endl;
+    }
+    return 0;
+}
